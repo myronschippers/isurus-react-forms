71846e1c50b239a9ce3838ea53e77ec8a37067eb
Myron <dubtwee@gmail.com>
2019-08-22 10:16:47 -0500

    submitting employee name to a list and clearing out the form submission

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,33 +5,11 @@ import Header from '../Header/Header';
 class App extends Component {
     state = {
         employee: {
-            name: 'Jim',
-            occupation: 'Software Developer',    
+            name: '',
+            occupation: '',    
         },
         employeesList: [],
     };
-
-    // handlerChangeName = (event) => {
-    //     const fieldValue = event.target.value;
-
-    //     this.setState({
-    //         employee: {
-    //             ...this.state.employee,
-    //             name: fieldValue,
-    //         }
-    //     })
-    // }
-
-    // handlerChangeOccupation = (event) => {
-    //     const fieldValue = event.target.value;
-
-    //     this.setState({
-    //         employee: {
-    //             ...this.state.employee,
-    //             occupation: fieldValue,
-    //         }
-    //     })
-    // }
 
     // One event handler for all input field change events
     handlerChangeOfAll(event, dataKey) {
@@ -47,23 +25,58 @@ class App extends Component {
         })
     }
 
+    handleClickAddEmployee = (event) => {
+        console.log(this.state.employee.name);
+        this.setState({
+            employee: {
+                name: '',
+                occupation: '',
+            },
+            employeesList: [
+                ...this.state.employeesList,
+                this.state.employee.name
+            ]
+        });
+    }
+
     render() {
+        let message = null;
+        const employeeListElements = [];
+        
+        if (this.state.employeesList.length === 0) {
+            message = 'There are no employees';
+        }
+
+        for (let i = 0; i < this.state.employeesList.length; i++) {
+            const employee = this.state.employeesList[i];
+            employeeListElements.push(<li key={i}>{employee}</li>);
+        }
+
         return (
             <div>
                 <Header title="React Forms" />
 
                 <div className="container">
                     <input
-                        type="text"
                         placeholder="Name"
                         onChange={(event) => this.handlerChangeOfAll(event, 'name')}
+                        value={this.state.employee.name}
                     />
                     <input
-                        type="text"
                         placeholder="Occupation"
                         onChange={(event) => this.handlerChangeOfAll(event, 'occupation')}
+                        value={this.state.employee.occupation}
                     />
+                    <button onClick={this.handleClickAddEmployee}>Add Employee</button>
                     <p>{this.state.employee.name} is a {this.state.employee.occupation}</p>
+                </div>
+
+                <div className="container">
+                    <h3>Employees</h3>
+                    <ul>
+                        {employeeListElements}
+                    </ul>
+                    {message}
                 </div>
             </div>
         );
